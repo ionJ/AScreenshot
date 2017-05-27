2a52a0e998120eea13b58db555bd2341a95b4023
ionJ <ion_J@outlook.com>
2017-05-27 18:11:54 +0800

    为 webView 添加了个悬浮菜单按钮
    依赖了 com.github.clans:fab:1.6.4 库，该按钮在悬浮按钮的基础上改进为悬浮菜单。触发事件后面再加

--- app/src/main/java/com/example/android/ascreenshot/MainActivity.java
+++ app/src/main/java/com/example/android/ascreenshot/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View.OnClickListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
